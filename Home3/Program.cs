ad7116b2bd0c8410fe853e63d422895226249c41
Dmitry-Lab <poznavshii@gmail.com>
2022-08-26 09:33:03 +0400

    Home3

---
+++ Home3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+Console.WriteLine(" ");
+
+while (index <= N)
+{
+    double sq = Math.Pow(index, 3);
+    Console.WriteLine(sq);
+    index++;
+}
