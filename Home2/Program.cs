6a1c84a640227ba0efef10c85803a940447b51a0
Dmitry-Lab <poznavshii@gmail.com>
2022-08-26 09:27:01 +0400

    Home2

---
+++ Home2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+//  и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите х1:");
+int xa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y1:");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите z1:");
+int za = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите х2:");
+int xb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y2:");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите z2:");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double S = Math.Sqrt(Math.Pow(xb - xa, 2)  + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+
+Console.WriteLine(S);
